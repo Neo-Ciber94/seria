fc1f0f846db9e3635bb2944ac2a7e7cbaf65a1d7
Freddy L <neo-ciber_94@hotmail.com>
2024-03-19 23:34:14 -0400

    Exclude test files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src"],
+  entry: ["src", "!src/**/*.test.*"],
   sourcemap: process.env.NODE_ENV === "development",
   minify: true,
   clean: true,
