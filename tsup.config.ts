ebdbe6d4266d83b9e1ede703520c2a0ab31e22cc
Freddy L <neo-ciber_94@hotmail.com>
2024-03-19 23:26:24 -0400

    Remove sourcemaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src"],
+  sourcemap: process.env.NODE_ENV === "development",
   minify: true,
-  sourcemap: true,
   clean: true,
   dts: true,
 });
