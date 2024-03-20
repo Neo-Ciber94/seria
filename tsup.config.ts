8f240fd82cae3b3dfb30502391075907231b97cb
Freddy L <neo-ciber_94@hotmail.com>
2024-03-20 18:51:47 -0400

    Add esm support

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src", "!src/**/*.test.*"],
-  sourcemap: process.env.NODE_ENV === "development",
-  minify: true,
+  sourcemap: true,
   clean: true,
   dts: true,
+  format: ["esm", "cjs"],
 });
