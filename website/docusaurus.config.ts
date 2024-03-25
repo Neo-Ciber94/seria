6eca72969e2d1baa80b9cab6ef2b8705ed656e78
Freddy L <neo-ciber_94@hotmail.com>
2024-03-25 01:07:27 -0400

    Update config

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -8,14 +8,14 @@ const config: Config = {
   favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://Neo-Ciber94.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/seria",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Neo-Ciber", // Usually your GitHub org/user name.
+  organizationName: "Neo-Ciber94", // Usually your GitHub org/user name.
   projectName: "seria", // Usually your repo name.
 
   onBrokenLinks: "throw",
