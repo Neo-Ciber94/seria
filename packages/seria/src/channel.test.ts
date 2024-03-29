2c36c6bd9aefebd3564fde891b96b9ab6b06266a
Freddy L <neo-ciber_94@hotmail.com>
2024-03-29 00:19:22 -0400

    Added channel

---
+++ packages/seria/src/channel.test.ts
@@ -0,0 +1,85 @@
+import { describe, expect, test } from "vitest";
+import { createChannel } from "./channel";
+
+describe("Channel", () => {
+  test("Should share id and context", () => {
+    const [sender, receiver] = createChannel<number>({
+      id: 99,
+      context: {
+        values: [1, 2, 3],
+      },
+    });
+
+    expect(sender.id).toStrictEqual(99);
+    expect(receiver.id).toStrictEqual(99);
+
+    expect(sender.context).toStrictEqual({
+      values: [1, 2, 3],
+    });
+
+    expect(receiver.context).toStrictEqual({
+      values: [1, 2, 3],
+    });
+  });
+
+  test("Should read channel values", async () => {
+    const [sender, receiver] = createChannel<number>({ id: 1 });
+
+    setImmediate(() => {
+      for (let i = 0; i <= 3; i++) {
+        sender.send(i);
+      }
+
+      sender.send(Promise.resolve(4));
+      sender.close();
+    });
+
+    await expect(receiver.recv()).resolves.toStrictEqual(0);
+    await expect(receiver.recv()).resolves.toStrictEqual(1);
+    await expect(receiver.recv()).resolves.toStrictEqual(2);
+    await expect(receiver.recv()).resolves.toStrictEqual(3);
+    await expect(receiver.recv()).resolves.toStrictEqual(4);
+    expect(receiver.recv()).toBeUndefined();
+  });
+
+  test("Should hold until value is received", async () => {
+    const [sender, receiver] = createChannel<string>({ id: 1 });
+
+    setTimeout(() => {
+      sender.send("Nobara");
+    }, 100);
+
+    await expect(receiver.recv()).resolves.toStrictEqual("Nobara");
+  });
+
+  test("Should throw if channel is closed", async () => {
+    const [sender, receiver] = createChannel<number>({ id: 1 });
+
+    setImmediate(() => {
+      sender.send(1);
+      sender.close();
+    });
+
+    await expect(receiver.recv()).resolves.toStrictEqual(1);
+    expect(() => sender.send(2)).toThrow();
+  });
+
+  test("Should iterate over values", async () => {
+    const [sender, receiver] = createChannel<number>({ id: 1 });
+
+    setImmediate(() => {
+      for (let i = 0; i < 3; i++) {
+        sender.send(i);
+      }
+
+      sender.close();
+    });
+
+    let idx = 0;
+    for await (const item of receiver) {
+      expect(item).toStrictEqual(idx++);
+    }
+
+    expect(receiver.recv()).toBeUndefined();
+  });
+});
---
+++ packages/seria/src/channel.ts
@@ -0,0 +1,93 @@
+type Sender<T, TContext = unknown> = {
+  id: number;
+  context?: TContext;
+  send: (value: T | Promise<T>) => void;
+  close: () => void;
+};
+
+type Receiver<T, TContext = unknown> = {
+  id: number;
+  context?: TContext;
+  recv: () => Promise<T> | undefined;
+  isClosed: () => boolean;
+  [Symbol.asyncIterator]: () => AsyncIterator<T>;
+};
+
+type ChannelOptions<TContext> = {
+  id: number;
+  context?: TContext;
+};
+/**
+ * Create a multi-producer and single consumer channel.
+ * @param options Options used to create the channel.
+ * @returns A tuple with the sender and receiver.
+ */
+export function createChannel<T, TContext = unknown>(
+  options: ChannelOptions<TContext>
+) {
+  const { id, context } = options;
+  const promiseQueue: Promise<T>[] = [];
+  let pendingResolve: ((value: T | Promise<T>) => void) | undefined = undefined;
+  let closed = false;
+
+  function send(value: T | Promise<T>) {
+    if (closed) {
+      throw new Error("Channel was closed");
+    }
+
+    const promise = value instanceof Promise ? value : Promise.resolve(value);
+
+    if (pendingResolve) {
+      pendingResolve(promise);
+      pendingResolve = undefined;
+      return;
+    }
+
+    promiseQueue.push(promise);
+  }
+
+  function recv() {
+    if (closed && promiseQueue.length === 0) {
+      return undefined;
+    }
+
+    return new Promise<T>((resolve) => {
+      const promise = promiseQueue.shift();
+      if (promise) {
+        resolve(promise);
+      } else {
+        pendingResolve = resolve;
+      }
+    });
+  }
+
+  async function* asyncIterator() {
+    while (true) {
+      const item = await recv();
+      if (item === undefined) {
+        return;
+      }
+
+      yield item;
+    }
+  }
+
+  const sender: Sender<T, TContext> = {
+    id,
+    context,
+    send,
+    close: () => {
+      closed = true;
+    },
+  };
+
+  const receiver: Receiver<T, TContext> = {
+    id,
+    context,
+    recv,
+    isClosed: () => closed,
+    [Symbol.asyncIterator]: asyncIterator,
+  };
+
+  return [sender, receiver] as const;
+}
